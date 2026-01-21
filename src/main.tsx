636a7cfc86088d9f47ec75decb1bc39618b1025b
Rahul Saha <76849680+RahulSahaSony@users.noreply.github.com>
2026-01-20 20:04:31 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
 import "./styles/globals.css";
+import App from "./app/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
