eaebad72c77c5db0459f074d8f3f10c1782eb494
Rahul Saha <76849680+RahulSahaSony@users.noreply.github.com>
2026-01-20 20:16:43 -0500

    Refactor Divider component to default export

--- src/components/UI/Divider.tsx
+++ src/components/UI/Divider.tsx
@@ -1,17 +1,3 @@
-// src/components/UI/Divider.tsx
-import { HTMLAttributes, forwardRef } from "react";
-import { cn } from "@/utils/helpers";
-
-interface DividerProps extends HTMLAttributes<HTMLDivElement> {}
-
-export const Divider = forwardRef<HTMLDivElement, DividerProps>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn("h-px w-full bg-border", className)}
-      {...props}
-    />
-  )
-);
-
-Divider.displayName = "Divider";
+export default function Divider() {
+  return <div className="divider my-6" />;
+}
