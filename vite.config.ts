a138e883221e3b3ca8d20c21c37383b4a843d167
Rahul Saha <76849680+RahulSahaSony@users.noreply.github.com>
2026-01-20 19:40:44 -0500

    Change base URL in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: 'rahulsahasony.github.io', // Replace with your repository name
+  base: '/', // Replace with your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
