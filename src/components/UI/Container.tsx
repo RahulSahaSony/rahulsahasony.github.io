9f184773c7acff30ab915563507e4f9e094f4026
Rahul Saha <76849680+RahulSahaSony@users.noreply.github.com>
2026-01-20 20:16:25 -0500

    Add Container component for layout purposes

---
+++ src/components/UI/Container.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export default function Container({ children }: { children: ReactNode }) {
+  return <div className="mx-auto w-full max-w-5xl px-4 sm:px-6">{children}</div>;
+}
