af244a6067f53b6be90647e035172da8224c9163
Rahul Saha <76849680+RahulSahaSony@users.noreply.github.com>
2026-01-21 19:50:06 -0500

    Delete src directory

--- src/components/UI/Container.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export default function Container({ children }: { children: ReactNode }) {
-  return <div className="mx-auto w-full max-w-5xl px-4 sm:px-6">{children}</div>;
-}
